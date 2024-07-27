186ff571d8d2c2d4e774fc1bee9fa1b9dea85157
sixface314 <sixface314@outlook.com>
2024-07-27 02:28:38 -0400

    Update script.js

--- script.js
+++ script.js
@@ -428,3 +428,4 @@ patrolSelect.addEventListener('change', updateCheckpointList);
 generateReportButton.addEventListener('click', generateReport);
 document.getElementById('view-past-reports').addEventListener('click', viewPastReports);
 document.getElementById('view-all-reports').addEventListener('click', viewAllReports);
+
